3f26f8358f6c31990a4835f92842b3a0364c18d3
AlisanK <Alisan.K@yandex.ru>
2017-12-10 20:17:53 +0300

    Create TreeNode.h

---
+++ Tree/TreeNode.h
@@ -0,0 +1,27 @@
+#pragma once
+
+
+template <class T> class TreeNode
+{
+	friend class Tree<T>;
+public:
+	TreeNode(const T &);
+	T get_value();
+protected:
+	TreeNode* left;           /* указатель на левого ребенка */
+	TreeNode* right;          /* указатель на правого ребенка */
+	TreeNode* parent;         /* указатель на родителя */
+	T value;            /* значение */
+};
+
+template<class T>
+TreeNode <T>::TreeNode(const T &a)
+{
+	value = a;
+	left = right = 0;
+}
+template <class T>
+T TreeNode <T>::get_value()
+{
+	return value;
+}
