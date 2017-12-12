59a9454bfacc2cff233fd1b678867853d9016625
AlisanK <Alisan.K@yandex.ru>
2017-12-12 17:01:40 +0300

    Create main.cpp

---
+++ Queue/main.cpp
@@ -0,0 +1,20 @@
+
+int main() {
+
+	/*Queue<int> Q;
+	int x;
+	for (int i = 0; i < 10; i++)
+	{
+		std::cin >> x;
+		Q.add_node(x);
+	}*/
+
+	int x;
+	std::cin >> x;
+	QNode<int> Qn(x);
+
+
+
+	system("pause");
+	return 0;
+}
