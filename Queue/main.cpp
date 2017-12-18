55c34c055b49f3c7f3b04bc91ffa9c3294173dd3
AlisanK <Alisan.K@yandex.ru>
2017-12-19 00:54:30 +0300

    Update main.cpp

--- Queue/main.cpp
+++ Queue/main.cpp
@@ -1,24 +1,16 @@
-
 #include <iostream>
 #include "Queue.h"
-#include "Q_node.h"
 
 int main() {
 
-	/*Queue<int> Q;
+	Queue<int> Q;
 	int x;
 	for (int i = 0; i < 10; i++)
 	{
 		std::cin >> x;
 		Q.add_node(x);
-	}*/
-
-	int x;
-	std::cin >> x;
-	QNode<int> Qn(x);
-	Qn.QNode_Dump();
-
-
+	}
 
 	system("pause");
 	return 0;
+}
